b7c61149021aa2246f823500ad7cc418b6c69235
Sagar-masale <sagarmasale27@gmail.com>
2024-09-26 00:29:45 +0530

    add controller and route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'events';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -8,6 +8,19 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+
+
+
+
+
+
+// routes import 
+import userRout from './routes/user.routes.js';
+
+
+// routes declaration
+app.use("/api/v1/users", userRout)
 
 app.use(express.json( {limit: "16kb"} ))
 app.use(express.urlencoded( {extended: true, limit: "16kb"} ))
