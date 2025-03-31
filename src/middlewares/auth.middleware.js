ad8408b5e6c2ee4811dbd65fc3ea32d2e43641b3
Sagar-masale <sagarmasale27@gmail.com>
2025-03-31 13:49:50 +0530

    update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import { apiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
-import { Admin } from "../models/Admin.model.js";
+import { Admin } from "../models/admin.model.js";
 
 
 
